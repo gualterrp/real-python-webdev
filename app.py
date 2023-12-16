744f24d6061ba5fd9398861afeda7952ceffd468
gualterrp <gualterrp@gmail.com>
2023-12-16 17:58:52 -0300

    Changes in app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+# error handling by debuging mode
+app.config["DEBUG"] = True
+
 # use the decorator function
 # to link the view function to a url
 @app.route('/')
@@ -12,7 +15,35 @@ app = Flask(__name__)
 
 # define the view using a function to return a string
 def hello_world():
-    return "Hello, World!"
+    return "Hello, World!?!?!?!?!?!?!?"
+
+@app.route('/test/<search_query>')
+def search(search_query):
+    return search_query
+
+@app.route('/integer/<int:value>')
+def int_value(value):
+    print(value + 1)
+    return "correct"
+
+@app.route('/float/<float:value>')
+def float_value(value):
+    print(value + 1)
+    return "correct"
+
+@app.route('/path/<path:value>')
+def path_value(value):
+    print(value)
+    return "Correct"
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return f"Hello, {name}", 200
+    else:
+        return "Not Found", 404
+
+
 
 # start the development server using the run method
 if __name__ == "__main__":
